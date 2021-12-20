8b2ce8aac812ad3d2be6f93db0f93d1f0a129157
ehanrspp <92194938+ehanrspp@users.noreply.github.com>
2021-12-20 10:13:21 +0700

    Add files via upload

---
+++ Proses Bismillah.cpp
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <windows.h>
+
+void tampilMenu();
+int inputMenu(int input);
+void kredit();
+void Exit();
+int inputExit(int input);
+
+int main(){
+	tampilMenu();
+	return 0;
+}
+
+void tampilMenu(){
+	int input;
+	system("cls");
+	printf("\t\t\t\t\t _______________________________________________\n");
+	printf("\t\t\t\t\t|                                               |\n");
+	printf("\t\t\t\t\t|         #######  #####  #####  #   #          |\n");
+	printf("\t\t\t\t\t|         #  #  #  #      #   #  #   #          |\n");
+	printf("\t\t\t\t\t|         #  #  #  #####  #   #  #   #          |\n");
+	printf("\t\t\t\t\t|         #  #  #  #      #   #  #   #          |\n");
+	printf("\t\t\t\t\t|         #  #  #  #####  #   #  #####          |\n");
+	printf("\t\t\t\t\t|                                               |\n");
+	printf("\t\t\t\t\t|                 SOS GotAChange                |\n");
+	printf("\t\t\t\t\t|      ===================================      |\n");
+	printf("\t\t\t\t\t|        PLAY    SCORE    CREDIT    Exit        |\n");
+	printf("\t\t\t\t\t|        (1)      (2)      (3)      (4)         |\n");
+	printf("\t\t\t\t\t|                                               |\n");
+	printf("\t\t\t\t\t|_______________________________________________|");
+	printf("\n\n\n\t\t\t\t\t\t inputkan angka (1,2,3, Atau 4) : ");
+	inputMenu(input);
+}
+
+int inputMenu(int input){
+	scanf("%d", &input);
+	switch (input){
+		case 1: printf("\nmantap");break;
+		case 2: printf("\njiwa");break;
+		case 3: kredit();break;
+		case 4: Exit();break;
+		default : printf("\nInput angka harus 1,2,atau 3!\n");system("pause");tampilMenu();break;
+	}
+	return input;
+}
+
+void kredit(){
+	system("cls");
+	printf("\t\t\t\t\t\t _______________________________\n");
+	printf("\t\t\t\t\t\t|                               |\n");
+	printf("\t\t\t\t\t\t|  Terimakasih telah memainkan  |\n");
+	printf("\t\t\t\t\t\t|  game besutan kami, kelompok  |\n");
+	printf("\t\t\t\t\t\t|  GotAChance. Semoga kalian    |\n");
+	printf("\t\t\t\t\t\t|  Terhibur :)                  |\n");
+	printf("\t\t\t\t\t\t|_______________________________|\n");
+	system("pause");
+	tampilMenu();
+}
+
+void Exit(){
+	int input;
+	system("cls");
+	printf("\t\t\t\t\t\t _____________________\n");
+	printf("\t\t\t\t\t\t|                     |\n");
+	printf("\t\t\t\t\t\t|   Apa Anda Yakin?   |\n");
+	printf("\t\t\t\t\t\t|   ===============   |\n");
+	printf("\t\t\t\t\t\t|                     |\n");
+	printf("\t\t\t\t\t\t|    (1)YA (2)TIDAK   |\n");
+	printf("\t\t\t\t\t\t|_____________________|\n");
+	printf("\n\t\t\t\t\t\t Input Exit :");
+	inputExit(input);
+}
+
+int inputExit(int input){
+	scanf("%d", &input);
+	if(input==2){
+		tampilMenu();
+	}
+	if(input!=1 && input!=2){
+		printf("\nInput harus berupa angka 1 atau 2!\n");
+		system("pause");
+		Exit();
+	}
+	return input;
+}

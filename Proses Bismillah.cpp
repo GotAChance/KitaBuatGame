29fed5b71f0f15de65c9ba2516d2d01082275468
Ghaniaja26 <95170644+Ghaniaja26@users.noreply.github.com>
2022-01-01 17:31:17 +0700

    Update Proses Bismillah.cpp

--- Proses Bismillah.cpp
+++ Proses Bismillah.cpp
@@ -18,7 +18,7 @@ int inputExit(int input);
 void UIinputnama1();
 void UIinputnama2();
 void tampilPapanPvP(char nama1[10], char nama2[10], int skor1, int skor2, int player, char papan[6][6]); // papan di mode player vs player dan player vs bot berbeda karena ada perbedaan di papan skor nya //
-void inputUser(int inputSO, int inputBaris, int inputKolom, int m, int n, int k, int player,char papan[][6]);
+void PrintPosisiInput(int m, int n, int k,int player,char papan[][6]);
 void playPlayervsPlayer();
 
 /* Modul untuk yang player vs komputer */
@@ -31,8 +31,11 @@ void Level3();
 int OlahInputSO(int x);
 int OlahInputBaris(int y);
 int OlahInputKolom(int z);
-int cekSkor(int i,int j,char papan[6][6]);
+int cekSkor(int m,int n,int k,char papan[][6]);
 int cekKotakPenuh(int cekPenuh, int i, int j);
+int ketikSO (int inputSO, int k);
+int ketikBaris(int inputBaris, int m);
+int ketikKolom(int inputKolom,int n);
 void max6Huruf(char nama[10]);
 
 	
@@ -242,7 +245,6 @@ void Level2(){
 	printf("\t\t\t\t\t\t \xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\n");
 	printf("\t\t\t\t\t\t     Player 1   -  Player 2 \n");
     printf("\t\t\t\t\t \xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\n");
-    printf("\t\t\t\t\t         _____________________________ \n");
     printf("\t\t\t\t\t        |     |     |     |     |     |\n");
     printf("\t\t\t\t\t      1 |  %c  |  %c  |  %c  |  %c  |  %c  |\n", papan[0][0], papan[0][1], papan[0][2], papan[0][3], papan[0][4]);
     printf("\t\t\t\t\t        |_____|_____|_____|_____|_____|\n");
@@ -257,7 +259,7 @@ void Level2(){
     printf("\t\t\t\t\t        |_____|_____|_____|_____|_____|\n");
     printf("\t\t\t\t\t        |     |     |     |     |     |\n");
     printf("\t\t\t\t\t      5 |  %c  |  %c  |  %c  |  %c  |  %c  |\n", papan[4][0], papan[4][1], papan[4][2], papan[4][3], papan[4][4]);
-    printf("\t\t\t\t\t        |_____|_____|_____|_____|_____|\n");
+    printf("\t\t\t\t\t        |     |     |     |     |     |\n");
     printf("\t\t\t\t\t \xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\n");
     printf("\t\t\t\t\t           1     2     3     4     5   \n\n");
     printf("\t\t\t\t\t                    (Kolom)           \n\n");
@@ -280,7 +282,6 @@ void Level3(){
 	printf("\t\t\t\t\t\t \xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\n");
 	printf("\t\t\t\t\t\t     Player 1   -  Player 2 \n");
     printf("\t\t\t\t\t \xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\n");
-    printf("\t\t\t\t\t      ___________________________________ \n");
 	printf("\t\t\t\t\t     |     |     |     |     |     |     |\n");
     printf("\t\t\t\t\t   1 |  %c  |  %c  |  %c  |  %c  |  %c  |  %c  |\n", papan[0][0], papan[0][1], papan[0][2], papan[0][3], papan[0][4], papan[0][5]);
     printf("\t\t\t\t\t     |_____|_____|_____|_____|_____|_____|\n");
@@ -298,7 +299,7 @@ void Level3(){
     printf("\t\t\t\t\t     |_____|_____|_____|_____|_____|_____|\n");
     printf("\t\t\t\t\t     |     |     |     |     |     |     |\n");
     printf("\t\t\t\t\t   6 |  %c  |  %c  |  %c  |  %c  |  %c  |  %c  |\n", papan[5][0], papan[5][1], papan[5][2], papan[5][3], papan[5][4], papan[5][5]);
-    printf("\t\t\t\t\t     |_____|_____|_____|_____|_____|_____|\n");
+    printf("\t\t\t\t\t     |     |     |     |     |     |     |\n");
     printf("\t\t\t\t\t \xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\n");
     printf("\t\t\t\t\t        1     2     3     4     5     6   \n\n");
     printf("\t\t\t\t\t                    (Kolom)                 \n\n");
@@ -339,8 +340,11 @@ void playPlayervsPlayer(){
 		player=2;
 	}
 	tampilPapanPvP( player1.nama,  player2.nama,  player1.skor,  player2.skor,  player, papan);
-    inputUser( inputSO,  inputBaris,  inputKolom,  m,  n,  k,  player,papan);
-    if (cekSkor(i,j,papan)==1){
+    k=ketikSO (inputSO, k);
+	m=ketikBaris(inputBaris, m);
+	n=ketikKolom(inputKolom, n);	
+	PrintPosisiInput( m,  n,  k,player, papan);
+    if (cekSkor(m,n,k,papan)==1){
     	if (player==1){
     		player1.skor++;
     		player--;
@@ -349,9 +353,66 @@ void playPlayervsPlayer(){
 			player--;
 		}
 	}
-    player++;
+	player++;
     cekPenuh++;
     cekKotakPenuh(cekPenuh,i,j);
+	}
+}
+
+int cekSkor(int m,int n,int k,char papan[6][6]){
+	if (k==83){
+			if ((papan[m][n-1])=='O' && (papan[m][n-2])=='S')
+		{
+			return 1;
+		}
+	        if ((papan[m][n+1]== 'O') && (papan[m][n+2] == 'S'))
+        {
+            return 1;
+        }
+        if((papan[m+1][n] == 'O') && (papan[m+2][n] == 'S'))
+        {
+            return 1;
+        }
+        if((papan[m-1][n] == 'O') && (papan[m-2][n] == 'S'))
+        {
+            return 1;
+        }
+        if((papan[m+1][n+1] == 'O') && (papan[m+2][n+2] == 'S'))
+        {
+            return 1;
+        }
+        if((papan[m-1][n-1] == 'O') && (papan[m-2][n-2] == 'S'))
+        {
+            return 1;
+        }
+        if((papan[m+1][n-1] == 'O') && (papan[m+2][n-2] == 'S'))
+        {
+            return 1;
+        }
+        if((papan[m-1][n+1] == 'O') && (papan[m-2][n+2] == 'S'))
+        {
+            return 1;
+        }	
+	}
+    else if (k==79){
+    	if((papan[m+1][n] == 'S') && (papan[m-1][n] == 'S'))
+        {
+            return 1;
+        }
+        if((papan[m][n+1] == 'S') && (papan[m][n-1] == 'S'))
+        {
+            return 1;
+        }
+        if((papan[m+1][n+1] == 'S') && (papan[m-1][n-1] == 'S'))
+        {
+            return 1;
+        }
+        if((papan[m+1][n-1] == 'S') && (papan[m-1][n+1] == 'S'))
+        {
+            return 1;
+        }	
+	}else {
+		return 0;
 	}
 }
 
@@ -391,7 +452,6 @@ void tampilPapanPvP(char nama1[10], char nama2[10], int skor1, int skor2, int pl
 	printf("\t\t\t\t\t\t     (%s)       (%s)   \n", nama1,nama2);
 	printf("\t\t\t\t\t\t        %d             %d    \n", skor1,skor2);
     printf("\t\t\t\t\t \xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\n");
-    printf("\t\t\t\t\t      ___________________________________ \n");
 	printf("\t\t\t\t\t     |     |     |     |     |     |     |\n");
     printf("\t\t\t\t\t   1 |  %c  |  %c  |  %c  |  %c  |  %c  |  %c  |\n", papan[0][0], papan[0][1], papan[0][2], papan[0][3], papan[0][4], papan[0][5]);
     printf("\t\t\t\t\t     |_____|_____|_____|_____|_____|_____|\n");
@@ -409,7 +469,7 @@ void tampilPapanPvP(char nama1[10], char nama2[10], int skor1, int skor2, int pl
     printf("\t\t\t\t\t     |_____|_____|_____|_____|_____|_____|\n");
     printf("\t\t\t\t\t     |     |     |     |     |     |     |\n");
     printf("\t\t\t\t\t   6 |  %c  |  %c  |  %c  |  %c  |  %c  |  %c  |\n", papan[5][0], papan[5][1], papan[5][2], papan[5][3], papan[5][4], papan[5][5]);
-    printf("\t\t\t\t\t     |_____|_____|_____|_____|_____|_____|\n");
+    printf("\t\t\t\t\t     |     |     |     |     |     |     |\n");
     printf("\t\t\t\t\t \xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\n");
     printf("\t\t\t\t\t        1     2     3     4     5     6   \n\n");
     printf("\t\t\t\t\t                    (Kolom)                 \n\n");
@@ -417,12 +477,7 @@ void tampilPapanPvP(char nama1[10], char nama2[10], int skor1, int skor2, int pl
     printf("\t\t\t\t\t  Ketikan Angka 1 untuk (S) atau angka 2 untuk (O) :");
 }
 
-void inputUser(int inputSO, int inputBaris, int inputKolom, int m, int n, int k, int player,char papan[][6]){
-	k=OlahInputSO (inputSO);
-    printf("\n\t\t\t\t\t  Masukan Posisi Baris :");
-    m=OlahInputBaris (inputBaris);
-    printf("\n\t\t\t\t\t  Masukan Posisi Kolom :");
-    n=OlahInputKolom (inputKolom);
+void PrintPosisiInput(int m, int n, int k,int player,char papan[][6]){
     if (papan[m][n]==' '){
    		 papan[m][n]=k;	
 	}else {
@@ -431,6 +486,27 @@ void inputUser(int inputSO, int inputBaris, int inputKolom, int m, int n, int k,
 		player--;
 	}
 }
+
+int ketikSO (int inputSO, int k){
+	scanf("%d", &inputSO);
+	k=OlahInputSO (inputSO);
+    printf("\n\t\t\t\t\t  Masukan Posisi Baris :");
+    return k;
+}
+
+int ketikBaris(int inputBaris, int m){
+	scanf("%d", &inputBaris);
+    m=OlahInputBaris (inputBaris);
+    printf("\n\t\t\t\t\t  Masukan Posisi Kolom :");
+    return m;
+}
+
+int ketikKolom(int inputKolom,int n){
+	scanf("%d", &inputKolom);
+    n=OlahInputKolom (inputKolom);
+    return n;
+}
+
 
 int cekKotakPenuh(int cekPenuh, int i, int j){
 	int penanda;
@@ -441,26 +517,8 @@ int cekKotakPenuh(int cekPenuh, int i, int j){
 	}
 	return penanda;
 }
-int cekSkor(int i,int j,char papan[6][6]){
-	int a,b;
-	for (a=0;a<i;a++){
-		for (b=0;b<j;b++){
-			if (papan[a][b]=='S' && papan[a][b+1]=='O' && papan[a][b+2]=='S'){
-				return 1;
-			}else if (papan[a][b]=='S' && papan[a+1][b+1]=='O' && papan[a+2][b+2]=='S'){
-				return 1;
-			}else if (papan[a][b]=='S' && papan[a+1][b]=='O' && papan[a+2][b]=='S'){
-				return 1;
-			}else if (papan[a][b]=='S' && papan[a-1][b+1]=='O' && papan[a-2][b+2]=='S'){
-				return 1;
-			}else {
-				return 0;
-			}
-		}
-	}
-}
+
 int OlahInputSO(int x){
-	scanf("%d", &x);
 	if(x==1){
 		x=83;
 	}else if (x==2){
@@ -470,13 +528,11 @@ int OlahInputSO(int x){
 }
 
 int OlahInputBaris(int y){
-	scanf("%d", &y);
 	y=y-1;
 	return y;
 }
 
 int OlahInputKolom(int z){
-	scanf("%d", &z);
 	z=z-1;
 	return z;
 }
